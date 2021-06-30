35ff512b097b06e6af7b316eb8408cdefbe6e453
Pranjal Gupta <pranjalg131@gmail.com>
2021-06-30 02:15:18 +0000

    Added the meals component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <>
       <Header />
+      <Meals />
     </>
   );
 }
